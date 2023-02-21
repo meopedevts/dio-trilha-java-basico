6e54a3a4f0dd45e0ae746b220d0bc020c14cd83e
Marcelo-SM <marcelo.meope@icloud.com>
2023-02-20 22:11:25 -0300

    Projeto Conta Terminal

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,15 @@
 public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
     
+
+        String resultado = a == b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
+
+    }
 }
